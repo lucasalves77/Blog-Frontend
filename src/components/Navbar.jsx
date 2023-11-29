e0c37a0011721d965ac57103b8f2b07a81d6e2c1
lucasalves77 <lucasalves77@proton.me>
2023-11-29 10:01:59 -0300

    ultimo commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,28 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom"
 import './Navbar.css';
-import minhaImagen from '../imagens/logo.png'
+import minhaImagen from '../imagens/logo.png';
+import {FaBars} from "react-icons/fa"
+import {ImCross} from "react-icons/im"
+
 
 
 
 const Navbar = () => {
+
+  const [Mobile, setMobile] = useState(false);
+
   return(
-    <div className="menu">
-      <nav className="container menu2">
-        <Link className="link" to="/Html">Html</Link>
-        <Link className="link" to="/Css">Css</Link>
-        <Link className="link" to="/Javascript">Javascript</Link>
-        <Link className="link" to="/Node">Node</Link>
-        <Link className="link" to="/React">React</Link>
-        <Link className="link" to="/Vue">Vue</Link>
-        <Link className="link" to="/Angular">Angular</Link>
-        <Link className="link" to="/Git">Git</Link>
-        <Link className="link" to="/Sass">Sass</Link>
-        <Link className="link" to="/Less">Less</Link>
-        <Link className="link" to="/Webpack">Webpack</Link>
-      </nav>
-      <div className="menuMobile">
-      <div className="openLogo">
-        <img src={minhaImagen} alt="" />
-        <div className="abrir" id="abrir" >
-          <i className="fa-solid fa-bars"></i>
+    <>
+      <div className="desktopMenu">
+           <div className="openLogo">
+          <img src={minhaImagen} alt="" />
+          <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)} >
+            {Mobile ? <ImCross/> : <FaBars/>}
+          </button>
         </div>
-      </div>
-        <div className="mobileMenu" id="mobileMenu">
-          <div className="fechar" >
-            <i className="fa-solid fa-xmark"></i>
-          </div>
-          <nav className="container">
-            <ul className="menuheader">
+          <nav className="container ">
+            <ul className={Mobile ? "menuLinksMobiles" : "menuLinks"} onClick={() => setMobile(false)}>
               <li><Link className="link" to="/Html">Html</Link></li>
               <li><Link className="link" to="/Css">Css</Link></li>
               <li><Link className="link" to="/Javascript">Javascript</Link></li>
@@ -46,10 +36,10 @@ const Navbar = () => {
               <li><Link className="link" to="/Webpack">Webpack</Link></li>
             </ul>
           </nav>
-        </div>
       </div>
-    </div> 
+    </>
   )
 }
 
 export default Navbar
+
