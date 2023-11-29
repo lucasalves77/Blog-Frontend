b31704a6cb04d4884db5d4dc545b307d531be257
lucasalves77 <lucasalves77@proton.me>
2023-11-29 18:16:41 -0300

    ultimo commit

---
+++ src/pages/MenuHeader.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import '../styles/MenuHeader.css'
+
+
+const MenuHeader = () => {
+  return(
+    <>
+    <div className="header">
+      <ul>
+        <li><Link className="header-link">Home</Link></li>
+        <li><Link className="header-link">Sobre</Link></li>
+        <li><Link className="header-link">Contato</Link></li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default MenuHeader
