3fdfe01501932c1f1997580ad9815493d29f421a
lucasalves77 <lucasalves77@proton.me>
2023-11-29 23:45:23 -0300

    ultimo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './Global.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from "./routes/Home"
-import Sobre from "./routes/Sobre"
-import Contato from "./routes/Contato"
-import Html from "./routes/Html";
-import Css from "./routes/Css";
-import Javascript from "./routes/Javascript";
-import Node from "./routes/Node";
-import Reactjs from "./routes/React";
-import Vue from "./routes/Vue";
-import Angular from "./routes/Angular";
-import Git from "./routes/Git";
-import Sass from "./routes/Sass";
-import Less from "./routes/Less";
-import Webpack from "./routes/Webpack";
+import Home from "./routes/Home/Home"
+import Sobre from "./routes/Sobre/Sobre"
+import Contato from "./routes/Contato/Contato"
+import Html from "./routes/Html/Html";
+import Css from "./routes/Css/Css";
+import Javascript from "./routes/Javascript/Javascript";
+import Node from "./routes/Node/Node";
+import Reactjs from "./routes/React/React";
+import Vue from "./routes/Vue/Vue";
+import Angular from "./routes/Angular/Angular";
+import Git from "./routes/Git/Git";
+import Sass from "./routes/Sass/Sass";
+import Less from "./routes/less/Less";
+import Webpack from "./routes/Webpack/Webpack";
 import Navbar from './components/Navbar';
 import Inicio from './components/Inicio'
 import MenuHeader from './pages/MenuHeader';
